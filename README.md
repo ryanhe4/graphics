ebb1798d6bc6868bdfb2957b091909bebfb807c4
heechan <ryanhe4@gmail.com>
2023-03-12 17:20:42 +0900

    add: deps spd log and glfw for window create

---
+++ README.md
@@ -0,0 +1,4 @@
+# install
+1. vcpkg 
+2. vcpkg install spdlog
+3. vcpkg install glfw3
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
+#include <GLFW/glfw3.h>
+#include <spdlog/spdlog.h>
+
 #include <iostream>
 
 int main(int argc, const char** argv) {
-  int t;
-  std::cin >> t;
-  std::cout << "hello, world"
-            << "\n";
+  SPDLOG_INFO("Start program");
+  if (!glfwInit()) {
+    const char* description = nullptr;
+    glfwGetError(&description);
+    SPDLOG_ERROR("failed to initalize glfw: {}", description);
+    return -1;
+  }
+
+  SPDLOG_INFO("Create glfw Window");
+  auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
+                                 nullptr, nullptr);
+  if (!window) {
+    SPDLOG_INFO("failed to create glfw window");
+    glfwTerminate();
+    return -1;
+  }
+
+  SPDLOG_INFO("Start main loop");
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
