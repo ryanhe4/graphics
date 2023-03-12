2808458d9ee2506a2c2641a7ddc1649f81fa7183
heechan <ryanhe4@gmail.com>
2023-03-12 18:02:55 +0900

    add: glad dep, for opengl functions

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 1. vcpkg 
 2. vcpkg install spdlog
 3. vcpkg install glfw3
+4. vcpkg install glad
+
+ glad가 glfw보다 먼저 include 되어야한다.
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <spdlog/spdlog.h>
 
 #include <iostream>
+
+void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
+  SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
+  glViewport(0,0,width,height);
+}
+
+void OnKeyEvent(GLFWwindow* window,
+    int key, int scancode, int action, int mods) {
+    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
+        key, scancode,
+        action == GLFW_PRESS ? "Pressed" :
+        action == GLFW_RELEASE ? "Released" :
+        action == GLFW_REPEAT ? "Repeat" : "Unknown",
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods & GLFW_MOD_ALT ? "A" : "-");
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
 int main(int argc, const char** argv) {
   SPDLOG_INFO("Start program");
@@ -12,18 +33,41 @@ int main(int argc, const char** argv) {
     return -1;
   }
 
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
   SPDLOG_INFO("Create glfw Window");
   auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
                                  nullptr, nullptr);
+
   if (!window) {
     SPDLOG_INFO("failed to create glfw window");
     glfwTerminate();
     return -1;
   }
 
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    SPDLOG_ERROR("failed to initialize glad");
+    glfwTerminate();
+    return -1;
+  }
+  auto glVersion = glGetString(GL_VERSION);
+  SPDLOG_INFO("OpenGL context version: {}", (char*)glVersion);
+
+  OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+  glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+  glfwSetKeyCallback(window, OnKeyEvent);
+
   SPDLOG_INFO("Start main loop");
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
+    glClearColor(0.0f,0.1f,0.2f,0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
   }
 
   glfwTerminate();
